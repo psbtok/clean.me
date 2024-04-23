c8b5edc2c964f3741ceba7baed5ab4921f81fc5b
petosbratok <opletaevpetr@mail.ru>
2024-04-23 15:35:58 +0300

    Login redesign

--- screens/login/Login.js
+++ screens/login/Login.js
@@ -1,72 +1,116 @@
-import React, { useState } from 'react';
-import { View, Text, Pressable, TextInput, StyleSheet } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useState, useRef } from 'react';
+import { ScrollView, View, Text, Pressable, TextInput, StyleSheet, Dimensions, Animated } from 'react-native';
 import { colors } from '../../constants/colors.js';
-import { ALERT_TYPE, Toast } from 'react-native-alert-notification';
+
+const screenHeight = Dimensions.get('window').height;
 
 export default function Login({ onLogin, baseUrl }) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleLogin = async () => {
-    try {
-      const response = await fetch(`${baseUrl}/login/`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
+  const scrollViewRef = useRef(null);
+  const [isSecondContainerVisible, setIsSecondContainerVisible] = useState(false);
+  const [showInputContainer, setshowInputContainer] = useState(false);
+  const toggleLoginScreen = () => {
+    setIsSecondContainerVisible(!isSecondContainerVisible);
+    if (scrollViewRef.current) {
+      scrollViewRef.current.scrollTo({
+        y: isSecondContainerVisible ? 0 : screenHeight,
+        animated: true,
       });
-  
-      if (response.status === 200) {
-        const data = await response.json();
-        const userId = data.id;
-        console.log("USERNAME SAVING");
-        await AsyncStorage.setItem('@MyApp:username', username);
-        console.log("ID SAVING");
-
-        await AsyncStorage.setItem('@MyApp:id', toString(userId));
-  
-        onLogin(true);
-        showToast(ALERT_TYPE.SUCCESS, 'Success', 'Login successful!');
-      } else {
-        console.error('Login failed:', response.statusText);
-        showToast(ALERT_TYPE.ERROR, 'Error', 'Login failed. Please try again.');
-      }
-    } catch (error) {
-      console.error('Error logging in:', error);
-      showToast(ALERT_TYPE.ERROR, 'Error', 'An error occurred while logging in.');
     }
   };
 
-  const showToast = (type, title, textBody) => {
-    Toast.show({
-      type,
-      title,
-      textBody,
-    });
-  };
-  
+  const showInputs = () => {
+    setshowInputContainer(true)
+    // Animated.timing(
+    //   fadeAnim, // The animated value to modify
+    //   {
+    //     toValue: 1, // Target opacity value (1 for fully opaque)
+    //     duration: 500, // Duration of the animation in milliseconds
+    //     useNativeDriver: true, // Use the native driver for performance
+    //   }
+    // ).start(); // Start the animation
+  };  
+
   return (
-    <View style={styles.container}>
-      <View style={styles.containerUpper}>
-          <Text style={styles.textMain}>Автомойки {'\n'}рядом.</Text>
-          <Pressable onPress={handleLogin} style={styles.button}>
-            <Text style={styles.buttonText}>Создать аккаунт</Text>
-          </Pressable>
+    <ScrollView
+      ref={scrollViewRef}
+      contentContainerStyle={styles.scrollContainer}
+      scrollEnabled={false}
+    >
+      <View style={styles.loginComponent}>
+        {/* First Container */}
+        <View style={[styles.container, styles.firstContainer]}>
+          <View style={styles.containerUpper}>
+            <Text style={styles.textMain}>Автомойки {'\n'}рядом.</Text>
+            {/* Phone Input */}
+            <Pressable onPress={showInputs} style={styles.button}>
+              <Text style={styles.buttonText}>Создать аккаунт</Text>
+            </Pressable>
+            <View 
+              style={[
+                styles.inputContainer, 
+                { opacity: showInputContainer ? 1 : 0 }
+              ]}
+            >
+              <TextInput
+                style={styles.input}
+                placeholder="Номер телефона"
+                keyboardType="phone-pad"
+                onChangeText={(text) => {}}
+              />
+              {/* SMS Code Input */}
+              <TextInput
+                style={styles.input}
+                placeholder="SMS-код (6 цифр)"
+                keyboardType="numeric"
+                maxLength={6}
+                onChangeText={(text) => {}}
+              />
+            </View>
+          </View>
+          <View style={styles.containerLower}>
+            <Text style={styles.textSub}>Уже есть аккаунт? </Text>
+            <Pressable onPress={toggleLoginScreen}>
+              <Text style={[styles.textSub, styles.linkText]}>Войти</Text>
+            </Pressable>
+          </View>
+        </View>        
+        <View style={styles.container}>
+          <View style={styles.containerUpper}>
+            <Text style={styles.textMain}>Автомойки {'\n'}рядом.</Text>
+            <Pressable style={styles.button}>
+              <Text style={styles.buttonText}>Создать аккаунт</Text>
+            </Pressable>
+          </View>
+          <View style={styles.containerLower}>
+            <Text style={styles.textSub}>Еще нет аккаунта? </Text>
+            <Pressable onPress={toggleLoginScreen}>
+              <Text style={[styles.textSub, styles.linkText]}>Регистрация</Text>
+            </Pressable>
+          </View>
+        </View>
       </View>
-      <View style={styles.containerLower}>
-        <Text style={styles.textSub}>Уже есть аккаунт? </Text>
-        <Text style={[styles.textSub, styles.linkText]}>Войти</Text>
-      </View>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  scrollContainer: {
+    justifyContent: 'center',
+    height: '200%',
+  },
+  loginComponent: {
+    height: '100%',
     flexDirection: 'column',
+  },
+  container: {
+    height: '50%',
+    flexDirection: 'column',
+  },
+  // firstContainer: {
+  //   marginTop: '-50%',
+  // },
+  inputContainer: {
+    width: '100%',
   },
   containerUpper: {
     padding: 24,
@@ -78,9 +122,9 @@ const styles = StyleSheet.create({
   containerLower: {
     height: 64,
     backgroundColor: colors.black,
-    flexDirection: 'row', // Set flexDirection to 'row'
+    flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center', // Align items vertically to center
+    alignItems: 'center',
   },
   textMain: {
     marginBottom: 24,
@@ -96,11 +140,19 @@ const styles = StyleSheet.create({
     paddingRight: 1,
     color: colors.grey4,
   },
+  input: {
+    width: '100%',
+    backgroundColor: '#FFFFFF',
+    padding: 12,
+    marginBottom: 10,
+    borderRadius: 10,
+  },
   button: {
     width: '100%',
     backgroundColor: colors.confirmBlue,
     padding: 12,
     paddingBottom: 14,
+    marginBottom: 10,
     borderRadius: 100,
   },
   buttonText: {
